0ab10f25d8d5ce4f32d6e30a6d4f352bf9b41b86
Angelo Dell Acqua <angelo.dellacqua82@gmail.com>
2025-08-15 03:05:36 -0400

    Skeleton

--- app/(dashboard)/dashboard/components/location-watcher.tsx
+++ app/(dashboard)/dashboard/components/location-watcher.tsx
@@ -5,6 +5,7 @@ import { loadNearbyStations } from '../lib/stations';
 import { Station } from './station';
 import { loadStationsStatus } from '@/app/(admin)/admin/lib/stations-status';
 import { loadStations } from '@/app/(admin)/admin/lib/stations';
+import { Skeleton } from '@/components/ui/skeleton';
 
 interface StationType {
   id: string;
@@ -15,14 +16,22 @@ interface StationType {
   num_docks_available: number;
 }
 
+enum LocStatus {
+  Waiting = 'Waiting for location...',
+  Error = 'Error processing location.',
+  Updated = 'Location updated, sending to server...',
+  DataReceived = 'Data received from server.',
+  NotSupported = 'Geolocation is not supported.',
+}
+
 export default function LocationWatcher() {
-  const [status, setStatus] = useState('Waiting for location...');
+  const [status, setStatus] = useState<LocStatus>(LocStatus.Waiting);
   const [locationData, setLocationData] = useState<StationType[]>([]);
   const [lastUpdate, setLastUpdate] = useState(0);
 
   useEffect(() => {
     if (!('geolocation' in navigator)) {
-      setStatus('Geolocation is not supported.');
+      setStatus(LocStatus.NotSupported);
       return;
     }
 
@@ -34,7 +43,7 @@ export default function LocationWatcher() {
         }
         setLastUpdate(now);
 
-        setStatus('Location updated, sending to server...');
+        setStatus(LocStatus.Updated);
         const { latitude, longitude } = position.coords;
 
         try {
@@ -42,21 +51,59 @@ export default function LocationWatcher() {
           await loadStationsStatus();
           const data = await loadNearbyStations(latitude, longitude);
           setLocationData(data);
-          setStatus('Data received from server.');
+          setStatus(LocStatus.DataReceived);
         } catch (err) {
           console.error(err);
-          setStatus('Error processing location.');
+          setStatus(LocStatus.Error);
         }
       },
       (error) => {
         console.error(error);
-        setStatus(`Error: ${error.message}`);
+        // setStatus(`Error: ${error.message}`);
       },
       { enableHighAccuracy: true, maximumAge: 5000, timeout: 10000 }
     );
 
     return () => navigator.geolocation.clearWatch(watchId);
   }, [lastUpdate]);
+
+  if (status === LocStatus.NotSupported) {
+    return (
+      <div className="p-4 rounded shadow">
+        Geolocation is not supported by your browser.
+      </div>
+    );
+  }
+
+  if (status === LocStatus.Error) {
+    return (
+      <div className="p-4 rounded shadow">Error retrieving location data.</div>
+    );
+  }
+
+  if (status === LocStatus.Waiting) {
+    return (
+      <div className="p-4 rounded shadow">
+        <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+        <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+        <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+        <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+        <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+      </div>
+    );
+  }
+
+  if (locationData.length === 0) {
+    return <div className="p-4 rounded shadow">No stations found nearby.</div>;
+  }
+
+  if (status === LocStatus.Updated) {
+    return (
+      <div className="p-4 rounded shadow">
+        Location updated, fetching nearby stations...
+      </div>
+    );
+  }
 
   return (
     <div className="p-4 rounded shadow">
