146cf6a0d5632df7274df43cfdf6b230d2ad4377
Angelo Dell Acqua <angelo.dellacqua82@gmail.com>
2025-08-15 01:35:37 -0400

    station component

--- app/(dashboard)/dashboard/components/station.tsx
+++ app/(dashboard)/dashboard/components/station.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
 import {
   Card,
   CardAction,
@@ -9,7 +10,15 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import { DialogHeader } from '@/components/ui/dialog';
 import { ScrollArea } from '@/components/ui/scroll-area';
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogTitle,
+  DialogDescription,
+} from '@/components/ui/dialog';
 
 interface LiveStationsProps {
   station: {
@@ -17,6 +26,7 @@ interface LiveStationsProps {
     name: string;
     distance: number;
     coordinates: [number, number];
+    distanceFormatted: string;
   };
 }
 
@@ -24,15 +34,29 @@ export function Station({ station }: LiveStationsProps) {
   return (
     <Card key={station.id}>
       <CardHeader>
-        <CardTitle>Card Title</CardTitle>
+        <CardTitle>{station.name}</CardTitle>
         <CardDescription>Card Description</CardDescription>
-        <CardAction>Card Action</CardAction>
+        <CardAction className="text-2xl">
+          {station.distanceFormatted}
+        </CardAction>
       </CardHeader>
       <CardContent>
         <p>Card Content</p>
-        <ScrollArea className="h-[200px] w-[350px] rounded-md border p-4">
-          <pre>{JSON.stringify(station, null, 2)}</pre>
-        </ScrollArea>
+        <Dialog>
+          <DialogTrigger>
+            <Button>details</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>details</DialogTitle>
+              <DialogDescription>
+                <ScrollArea className="h-[200px] min-w-screen rounded-md border p-4">
+                  <pre>{JSON.stringify(station, null, 2)}</pre>
+                </ScrollArea>
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
       </CardContent>
       <CardFooter>
         <p>Card Footer</p>
