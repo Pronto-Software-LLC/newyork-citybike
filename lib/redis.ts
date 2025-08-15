4118d6091093684d554d34b4075cc66ddc664366
Angelo Dell Acqua <angelo.dellacqua82@gmail.com>
2025-08-14 23:10:48 -0400

    fixing valkey setup

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,8 +1,8 @@
 import Valkey from 'iovalkey';
 
 export const redis = new Valkey({
-  port: 6379, // Valkey port
-  host: '127.0.0.1', // Valkey host
+  port: process.env.VALKEY_PORT, // Valkey port
+  host: process.env.VALKEY_HOST, // Valkey host
   // username: 'default', // needs Valkey >= 6
   // password: 'my-top-secret',
   db: 0, // Defaults to 0
