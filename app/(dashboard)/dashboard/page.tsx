19c8a86b6ac85908cdb65198b3828c0ecd9adfc6
Angelo Dell Acqua <angelo.dellacqua82@gmail.com>
2025-08-24 18:18:18 -0400

    added useContext to pass mapToUse parameter to directions button

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,9 +1,11 @@
+import { loadSettings } from '../settings/lib/save-settings';
 import LocationWatcher from './components/location-watcher';
 
 export default async function Home() {
+  const mapToUse = await loadSettings();
   return (
     <main>
-      <LocationWatcher />
+      <LocationWatcher mapToUse={mapToUse} />
     </main>
   );
 }
